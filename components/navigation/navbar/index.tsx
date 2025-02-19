1e1a7b2c5f06ed91ade8e0146cecf9225a06e7e7
otaja <marco.schottke@otaja.de>
2025-02-19 11:12:18 +0100

    add light/dark navbar switch

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav>Navbar</nav>
-  )
-}
+    <nav
+      className="flex-between background-light900_dark200 
+    fixed z-50 w-full p-6 dark:shadow-none 
+    sm:px-12"
+    >
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="Dev Flow Logo"
+        />
+        <p className="h2-bold dark: font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev <span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
+      <p>Global Seacrh</p>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
