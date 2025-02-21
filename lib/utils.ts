ff554c94d968003cc5d3590fc543cb4ba24447cf
otaja <marco.schottke@otaja.de>
2025-02-21 18:03:36 +0100

    add RightSidebar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getDeviconClaasName = (techName: string) => {
+  const normalizedTechName = techName.replace(/[ .]/g, "").toLowerCase();
+
+  const techMap: { [key: string]: string } = {
+    javascript: "devicon-javascript-plain",
+    typescript: "devicon-typescript-plain",
+    react: "devicon-react-original",
+    vue: "devicon-vuejs-plain",
+    angular: "devicon-angularjs-plain",
+    svelte: "devicon-svelte-plain",
+  };
+
+  return techMap[normalizedTechName]
+    ? `${techMap[normalizedTechName]} colored`
+    : "devicon-devicon-plain";
+};
